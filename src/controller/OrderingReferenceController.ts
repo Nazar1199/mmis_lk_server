f7edea601c2075aad836d5c435988500bf9f68b2
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-03-24 23:45:50 +0300

    feat: added ordering reference

---
+++ src/controller/OrderingReferenceController.ts
@@ -0,0 +1,71 @@
+import {NextFunction, Request, Response} from "express";
+import OrderingReferenceService from "../services/OrderingReferenceService";
+
+export class OrderingReferencesController {
+
+    static createOrderingReferences = async(request: Request, response: Response, next: NextFunction) => {
+        try {
+            let result = await OrderingReferenceService.createOrderingReference(request.body);
+            response.status(201).send(result);
+        } catch(error) {
+            response.status(500).send("Ошибка при добавлении заказанной справки: " + error);
+        }
+    }
+
+    static getAllOrderingReferences = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            let result = await OrderingReferenceService.getAllOrderingReferences();
+            response.status(200).send(result);
+        } catch(error) {
+            console.log("Error: " + error);
+            response.status(500).send("Не удалось получить список заказанных справок " + error);
+        }
+    }
+
+    static getOrderingReferenceById = async(request: Request, response: Response, next: NextFunction) => {
+        if (!request.params.id) {
+            throw new Error("Не удалось получить информацию о заказанной справке");
+        }
+        try {
+            console.log("Id ordering reference: " + request.params.id);
+            let result = await OrderingReferenceService.getOrderingReferenceById(request.params.id);
+            response.status(200).send(result);
+        } catch(error) {
+            response.status(500).send("Не удалось получить информацию о заказанной справке " + error);
+        }
+    }
+
+    static getAllOrderingReferencesForStudent = async(request: Request, response: Response, next: NextFunction) => {
+        if (!request.params.student) {
+            throw new Error("Не удалось получить информацию о заказанной справке");
+        }
+        try {
+            console.log("Id ordering reference: " + request.params.id);
+            let result = await OrderingReferenceService.getAllOrderingReferencesForStudent(request.params.student);
+            response.status(200).send(result);
+        } catch(error) {
+            response.status(500).send("Не удалось получить информацию о заказанной справке " + error);
+        }
+    }
+
+    static saveOrderingReference = async(request: Request, response: Response, next: NextFunction) => {
+        try {
+            let result = await OrderingReferenceService.saveOrderingReference(request.body);
+            response.status(200).send(result);
+        } catch(error) {
+            response.status(500).send("Не удалось сохранить информацию о заказанной справке " + error);
+        }
+    }
+
+    static deleteOrderingReference = async(request: Request, response: Response, next: NextFunction) => {
+        if (!request.params.id) {
+            throw new Error("Не удалось найти заказанную справку, которую требуется удалить");
+        }
+        try {
+            await OrderingReferenceService.deleteOrderingReference(request.params.id);
+            response.status(200).send();
+        } catch (error) {
+            response.status(500).send("Не удалось удалить заказанную справку" + error);
+        }
+    }
+}
