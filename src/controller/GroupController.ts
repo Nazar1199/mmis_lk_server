e9141fd1bbf282f27dee683ef5b5d436de5222d6
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-03-24 22:48:46 +0300

    feat: added new methods and information messages

--- src/controller/GroupController.ts
+++ src/controller/GroupController.ts
@@ -7,7 +7,7 @@ export class GroupController {
             let result = await GroupService.createGroup(request.body);
             response.status(200).send(result);
         } catch(error) {
-            response.status(500).send(error);
+            response.status(500).send("Ошибка при сохрании новой группы: " + error);
         }                
     }
 
@@ -16,7 +16,7 @@ export class GroupController {
             let result = await GroupService.getAllGroups();
             response.status(200).send(result);
         } catch(error) {
-            response.status(500).send(error);
+            response.status(500).send("Не удалось получить список групп: " + error);
         }         
     }
 
@@ -25,7 +25,25 @@ export class GroupController {
             let result = await GroupService.getGroupById(request.params.id);
             response.status(200).send(result);
         } catch(error) {
-            response.status(500).send(error);
+            response.status(500).send("Не удалось получить информацию о группе: " + error);
         }
+    }
+
+    static saveGroup = async(request: Request, response: Response, next: NextFunction) => {
+        try {
+            let profile = await GroupService.saveGroup(request.body);
+            response.status(200).send(profile);
+        } catch(error) {
+            response.status(500).send("Не удалось сохранить информацию о группе: " + error);
+        }         
+    }
+
+    static deleteGroup = async (request: Request, response: Response, next: NextFunction) => {
+        try {
+            let result = await GroupService.deleteGroup(request.params.id);
+            response.status(200).send(result);
+        } catch(error) {
+            response.status(500).send("Не удалось удалить группу: " + error);
+        }         
     }
 }
