68fe3164d30689da90fab7d6ab820237e84b4eec
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-04-28 14:49:16 +0300

    feat: added jwt to profile routes

--- src/routes/profile.routes.ts
+++ src/routes/profile.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { ProfileController } from "../controller/ProfileController";
+import checkJwtAuth from "../middleware/jwt";
 
 const ProfileRoutes = Router();
 
-ProfileRoutes.get("/:id", ProfileController.getProfileById);
+ProfileRoutes.get("/:id", checkJwtAuth, ProfileController.getProfileById);
 ProfileRoutes.post("/login", ProfileController.login);
 ProfileRoutes.post("/registration", ProfileController.registration)
-ProfileRoutes.post("/", ProfileController.createProfile);
-ProfileRoutes.put("/:id", ProfileController.saveProfile);
-ProfileRoutes.delete("/:id", ProfileController.deleteProfile);
+ProfileRoutes.post("/", checkJwtAuth, ProfileController.createProfile);
+ProfileRoutes.put("/:id", checkJwtAuth, ProfileController.saveProfile);
+ProfileRoutes.delete("/:id", checkJwtAuth, ProfileController.deleteProfile);
 
 export default ProfileRoutes;
