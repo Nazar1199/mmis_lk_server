438eb97dabd20a36d5dc87219410477c21f690d5
Nazar Ignatenko <nazar.ignatenkoru@gmail.com>
2023-04-28 17:09:57 +0300

    feat: added middleware

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { StudentController } from "../controller/StudentController";
+import checkJwtAdmin from "../middleware/jwtAdmin";
 
 const StudentRoutes = Router();
 
-StudentRoutes.get("/", StudentController.getAllStudents);
-StudentRoutes.get("/:id", StudentController.getStudentById);
-StudentRoutes.get("/group/:id", StudentController.getAllStudentsForGroup);
-StudentRoutes.post("/", StudentController.createStudent);
-StudentRoutes.put("/:id", StudentController.saveStudent);
-StudentRoutes.delete("/:id", StudentController.deleteStudent);
+StudentRoutes.get("/", checkJwtAdmin, StudentController.getAllStudents);
+StudentRoutes.get("/:id", checkJwtAdmin, StudentController.getStudentById);
+StudentRoutes.get("/group/:id", checkJwtAdmin, StudentController.getAllStudentsForGroup);
+StudentRoutes.post("/", checkJwtAdmin, StudentController.createStudent);
+StudentRoutes.put("/:id", checkJwtAdmin, StudentController.saveStudent);
+StudentRoutes.delete("/:id", checkJwtAdmin, StudentController.deleteStudent);
 
 export default StudentRoutes;
